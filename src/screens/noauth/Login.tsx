7fa0f38552cb8d796d5bd60961b3f4ccca09e28a
Gideon <owolabigidslab@gmail.com>
2023-01-09 15:03:38 +0100

    added signup and login screens boilerplate

---
+++ src/screens/noauth/Login.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type LoginProps = {};
+
+export const Login = ({}: LoginProps): JSX.Element => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
---
+++ src/screens/noauth/Signup.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type SignupProps = {};
+
+export const Signup = ({}: SignupProps): JSX.Element => {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
