aaebb8294611a4d0d53c9c782ebcca5fd402a7f4
Gideon <owolabigidslab@gmail.com>
2023-01-15 19:13:45 +0100

    added @react-native-firebase/crashlytics

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -100,7 +100,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     // borderWidth: 1,
     alignItems: 'center',
-    marginHorizontal: 10,
+    marginHorizontal: 20,
   },
   overlay: {
     padding: 0,
